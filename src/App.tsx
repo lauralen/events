c4cc52e49c82cd5b4acdea261046041329e1f0a7
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-04-22 17:58:58 +0300

    fetch events

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Input from './components/Input'
+const { REACT_APP_API_KEY } = process.env
+
+const BASE_URL = 'https://www.skiddle.com/api/v1/events/'
+
+type Status = 'idle' | 'loading' | 'failed'
+type UiStatus = Status | 'noResults'
 
 function App() {
+  const [events, setEvents] = useState<any>()
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [status, setStatus] = useState<Status>('idle')
+
+  useEffect(() => {
+    async function fetchEvents(): Promise<any> {
+      setStatus('loading')
+      try {
+        const result = await fetch(
+          `${BASE_URL}search/?api_key=${REACT_APP_API_KEY}&keyword=${searchValue}`
+        )
+        const data = await result.json()
+        const formattedEvents = data.results.map(
+          // @ts-ignore
+          ({ id, description, eventname, startdate, venue }) => {
+            return {
+              id,
+              description,
+              eventname,
+              startdate,
+              venuename: venue.name,
+            }
+          }
+        )
+        setEvents(formattedEvents)
+        setStatus('idle')
+      } catch (error) {
+        setStatus('failed')
+      }
+    }
+
+    fetchEvents()
+  }, [searchValue])
+
+  const getUiStatus = (): UiStatus => {
+    if (status === 'idle' && !events.length) {
+      return 'noResults'
+    } else {
+      return status
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>skiddle</div>
+        <Input
+          value={searchValue}
+          placeholder="search for events"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
       </header>
+      <main>
+        {
+          {
+            loading: <p>Loading...</p>,
+            failed: <p>Failed to load events</p>,
+            noResults: <p>No events found</p>,
+            idle: <div>events</div>,
+          }[getUiStatus()]
+        }
+      </main>
     </div>
   )
 }
