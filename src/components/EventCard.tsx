f4eb43e3235ac26a5c0fd577880584c6ce79f0fd
Laura Lenkutytė <laurallenkutyte@gmail.com>
2022-04-23 10:38:18 +0300

    fix event vard styling, format date

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import { Link, generatePath } from 'react-router-dom'
+import { format, parseISO } from 'date-fns'
+
 import { Event } from '../types'
 
 interface Props {
@@ -10,20 +12,20 @@ const Input: FC<Props> = ({ data }) => {
   const { id, description, eventname, startdate, venuename, imageurl } = data
 
   return (
-    <div className="bg-white">
-      <img className="w-full" alt="" src={imageurl} />
+    <div className="bg-white flex flex-col">
+      <img alt="" src={imageurl} />
 
-      <div className="p-5 flex flex-col items-center justify-between">
+      <div className="h-full p-5 flex flex-col items-center justify-between">
         <div>
           <h2 className="text-center text-lg">{eventname}</h2>
           <p>{description}</p>
           <p>{venuename}</p>
-          <p>{startdate}</p>
+          <p>{format(parseISO(startdate), 'do LLLL')}</p>
         </div>
 
         <Link
           to={generatePath('event/:id', { id })}
-          className="mt-4 p-2 rounded-sm bg-cyan-400 text-white"
+          className="w-full text-center mt-4 p-2 rounded-sm bg-cyan-400 text-white"
         >
           view details
         </Link>
